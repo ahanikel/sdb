43bffba64c45c672a46e36ccab900eac251d5e16
Axel Hanikel <ahanikel@adobe.com>
2018-09-05 16:03:13 +0200

    Add help screen

--- app/Main.hs
+++ app/Main.hs
@@ -20,3 +20,13 @@ main = do
     ["index",    tarFile]     -> index tarFile
     ["graph",    tarFile]     -> graph tarFile
     ["binaries", tarFile]     -> binaries tarFile
+    _                         -> usage
+
+usage = putStrLn $ unlines [ "sdb tars     <dir> [--all]  # List tarfiles in dir (default '.')."
+                           , "sdb entries  <tarFile>      # List entries in tarfile."
+                           , "sdb segments <tarFile>      # List segments in tarfile."
+                           , "sdb segment  <tarFile> <id> # display records in segment."
+                           , "sdb index    <tarFile>      # display tarfile index."
+                           , "sdb graph    <tarFile>      # display graph file in tar."
+                           , "sdb binaries <tarFile>      # display binary references in tar."
+                           ]
