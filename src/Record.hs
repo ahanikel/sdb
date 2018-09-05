8ae0d3f088e6ee4a0228466501452cb1c6fe4a6e
Axel Hanikel <ahanikel@gmail.com>
2018-09-06 00:23:58 +0200

    Adding the Record type (WIP)

--- src/Record.hs
+++ src/Record.hs
@@ -1,7 +1,8 @@
 module Record (RecordType(..), recordTypeFromWord8) where
 
-import Data.Word
-import Display
+import qualified Data.ByteString.Lazy as BL
+import           Data.Word
+import           Display
 
 data RecordType = RecTypeMapLeaf
                 | RecTypeMapBranch
@@ -30,3 +31,41 @@ recordTypeFromWord8 = (recTypes !!) . fromIntegral
                , RecTypeNode
                , RecTypeBlobID
                ]
+
+data ValueType = SmallValue
+               | MediumValue
+               | LargeValue
+               | ExternalValue
+
+type Reference = String
+
+data Record = BlockRecord         { recData      :: BL.ByteString
+                                  }
+            | ValueRecord         { recValueType :: ValueType
+                                  , recLength    :: Word64
+                                  , recData      :: BL.ByteString
+                                  }
+            | ListRecord          { recLength    :: Word64
+                                  , recBucket    :: Reference
+                                  }
+            | BucketRecord        { recLength    :: Word64
+                                  , recRecs      :: [Reference]
+                                  }
+            | MapLeafRecord       { recLength    :: Word64
+                                  , recPairs     :: [(String, Reference)]
+                                  }
+            | MapBranchRecord     { recLength    :: Word64
+                                  , recRecs      :: [Reference]
+                                  }
+            | TemplateZeroRecord  { recProps     :: [(Reference, Reference)]
+                                  }
+            | TemplateOneRecord   { recProps     :: [(Reference, Reference)]
+                                  , recChildren  :: [Reference]
+                                  }
+            | TemplateManyRecord  { recProps     :: [(Reference, Reference)]
+                                  , recChildren  :: [Reference]
+                                  }
+            | NodeRecord          { recTemplate  :: Reference
+                                  , recValues    :: Reference -- to a list record
+                                  , recTree      :: Reference -- to a map record
+                                  }
